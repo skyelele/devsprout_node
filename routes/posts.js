cc70c1e0d4aeb0c04c64a7ead97c16b9d5aa4b79
Skye Leahy <skyeleahy@protonmail.com>
2019-02-23 09:02:04 +0000

    modify posts.js controller and route

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { errorHandler } = require('../middleware');
-const { getPosts, newPost } = require('../controllers/posts');
+const { 
+  getPosts,
+  newPost,
+  createPost
+} = require('../controllers/posts');
 
 /* GET posts index /posts */
 router.get('/', errorHandler(getPosts));
@@ -10,28 +14,26 @@ router.get('/', errorHandler(getPosts));
 router.get('/new', newPost);
 
 /* POST posts create /posts */
-router.post('/', (req, res, next) => {
-  res.send('CREATE /posts');
-});
+router.post('/', errorHandler(createPost));
 
 /* GET posts show /posts/:id */
 router.get('/:id', (req, res, next) => {
-  res.send('SHOW /posts/:id')
+  res.send('SHOW /posts/:id');
 });
 
 /* GET posts edit /posts/:id/edit */
 router.get('/:id/edit', (req, res, next) => {
-  res.send('EDIT /posts/:id/edit')
+  res.send('EDIT /posts/:id/edit');
 });
 
 /* PUT posts update /posts/:id */
 router.put('/:id', (req, res, next) => {
-  res.send('UPDATE /posts/:id')
+  res.send('UPDATE /posts/:id');
 });
 
 /* DELETE posts destroy /posts/:id */
 router.delete('/:id', (req, res, next) => {
-  res.send('DELETE /posts/:id')
+  res.send('DELETE /posts/:id');
 });
 
 module.exports = router;
