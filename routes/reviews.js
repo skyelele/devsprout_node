b6b1fbc9e2db9fa16622cd845143889162d8283c
Skye Leahy <skyeleahy@protonmail.com>
2019-02-24 06:40:52 +0000

    add review create code

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,20 @@
 const express = require('express');
-// mergeParams pulls the id from app.js via app.use()
 const router = express.Router({ mergeParams: true });
+const { asyncErrorHandler } = require('../middleware');
+const {
+  reviewCreate,
+  reviewUpdate,
+  reviewDestroy
+} = require('../controllers/reviews');
 
 /* review reviews create /posts/:id/reviews */
-router.post('/', (req, res, next) => {
-  res.send('CREATE /posts/:id/reviews');
-});
+router.post('/', asyncErrorHandler(reviewCreate));
 
 /* PUT reviews update /reviews/:review_id */
-router.put('/:review_id', (req, res, next) => {
-  res.send('UPDATE /posts/:id/reviews/:review_id');
-});
+router.put('/:review_id', asyncErrorHandler(reviewUpdate));
 
 /* DELETE reviews destroy /posts/:id/reviews/:review_id */
-router.delete('/:review_id', (req, res, next) => {
-  res.send('DELETE /posts/:id/reviews/:review_id');
-});
+router.delete('/:review_id', asyncErrorHandler(reviewDestroy));
 
 module.exports = router;
 
