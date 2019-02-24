58254029c9cd3a9decec95dbe8da4fe6d9b779f2
Skye Leahy <skyeleahy@protonmail.com>
2019-02-24 05:09:37 +0000

    add layout to post's show.ejs view and also add boilerplate layout to all other views

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const createError = require('http-errors');
 const express = require('express');
+const engine = require('ejs-mate');
 const path = require('path');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
@@ -28,6 +29,8 @@ db.once('open', () => {
   // we're connected!
 });
 
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
---
+++ public/javascripts/post-show.js
@@ -0,0 +1,21 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2t5ZWxlbGUiLCJhIjoiY2pzaWEydTNoMGhvYTQzbm1wc3JzajdxaiJ9.p99L1ltEUrqrHvwSfBZFiA';
+        
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: post.coordinates,
+  zoom: 5
+});
+
+// add markers to map
+
+// create a HTML element for our post location/marker
+var el = document.createElement('div');
+el.className = 'marker';
+
+// make a marker for our location and add to the map
+new mapboxgl.Marker(el)
+.setLngLat(post.coordinates)
+.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+.setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
+.addTo(map);
